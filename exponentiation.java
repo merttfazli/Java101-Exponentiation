4e3a72c5b5f5373cc6872340b46d36d6c8d4e7a5
Mert Fazlı Saylak <76404240+merttfazli@users.noreply.github.com>
2023-02-28 10:49:07 +0300

    Create exponentiation.java

---
+++ exponentiation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exponentiation {
+    public static int exponentiation(int base,int power){
+        if(power==0){
+            return 1;
+        } else if (base==1) {
+            return 1;
+        }
+        else
+        {
+            return base*exponentiation(base,power-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Tabanı Giriniz: ");
+        int base = scan.nextInt();
+        System.out.print("Üssü Giriniz: ");
+        int power=scan.nextInt();
+        System.out.println(base+" üzeri "+power+" işleminin sonucu : "+exponentiation(base,power));
+    }
+}
